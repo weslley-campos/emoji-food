501421405f74a18d444004064667b67d55e07a96
Weslley Campos <weslley_campos@outlook.com>
2024-10-01 22:38:01 -0300

    🐞 fix: ran detekt to fix lint issues.

--- app/src/main/java/com/github/weslleystos/navigation/ui/theme/Theme.kt
+++ app/src/main/java/com/github/weslleystos/navigation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.github.weslleystos.navigation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +20,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -30,7 +28,7 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+     */
 )
 
 @Composable
